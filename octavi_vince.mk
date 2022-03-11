4ad888c30e77a9e41b9ef61160563157fd0ff8dc
GhostMaster69-dev <rathore6375@gmail.com>
2022-03-12 00:48:51 +0530

    vince: Octavi-ify
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- octavi_vince.mk
+++ octavi_vince.mk
@@ -27,16 +27,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
-TARGET_BOOT_ANIMATION_RES := 1440
-
-# Build Official
-REVENGEOS_BUILDTYPE := OFFICIAL
+# Inherit some common OctaviOS stuff.
+$(call inherit-product, vendor/octavi/config/common.mk)
+OCTAVI_BUILD_TYPE := OFFICIAL
+OCTAVI_DEVICE_MAINTAINER := GhostMaster69-dev
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := revengeos_vince
+PRODUCT_NAME := octavi_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
